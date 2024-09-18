2d05d64c2467d0c4ff4fd8feae213c5b6d0ebb8e
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2024-09-18 15:20:55 +0530

    upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { RolesComponent } from './components/roles/roles.component';
 import { DesignationComponent } from './components/designation/designation.component';
 import { MasterComponent } from './components/master/master.component';
+import { ClientComponent } from './components/client/client.component';
+import { EmployeeComponent } from './components/employee/employee.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RolesComponent,MasterComponent],
+  imports: [RouterOutlet, RolesComponent, MasterComponent, ClientComponent, DesignationComponent, EmployeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { IDesignation } from '../../model/interface/designation';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { MasterService } from '../../services/master.service';
 
 @Component({
   selector: 'app-designation',
@@ -10,19 +11,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './designation.component.html',
   styleUrl: './designation.component.css'
 })
-export class DesignationComponent {
+export class DesignationComponent implements OnInit {
   designations: IDesignation[] = [];
   http = inject(HttpClient);
+  masterService = inject(MasterService);
+
+  isLoading: boolean = true;
 
   ngOnInit(): void {
     this.getAllDesignations();
   }
   getAllDesignations(): void {
-    this.http.get('https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllDesignation').subscribe((response: any) => {
+    this.masterService.getDesignations().subscribe((response: any) => {
+      this.isLoading = false;
       this.designations = this.assignColors(response.data);
     });
   }
-
   assignColors(roles: IDesignation[]): IDesignation[] {
     const colors = [
       'bg-purple-500',
